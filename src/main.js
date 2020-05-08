4db66786a0bf6803504a2726ec46de61384f65ac
Alexander <al.deimosfrontend@gmail.com>
2020-05-08 12:15:24 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const siteHeaderInfo = document.querySelector(`.trip-info`);
+const siteHeaderMenu = document.querySelector(`.trip-controls`);
+
+const render = (container, template, place = `beforeend`) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const createRouteComponent = () => {
+  return (
+    `<div class="trip-info__main">
+              <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+
+              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
+            </div>`
+  );
+};
+
+const createSiteMenuComponent = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+              <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+              <a class="trip-tabs__btn" href="#">Stats</a>
+            </nav>`
+  );
+};
+
+const createSiteFilterComponent = () => {
+  return (
+    `<form class="trip-filters" action="#" method="get">
+              <div class="trip-filters__filter">
+                <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+                <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+              </div>
+
+              <div class="trip-filters__filter">
+                <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+                <label class="trip-filters__filter-label" for="filter-future">Future</label>
+              </div>
+
+              <div class="trip-filters__filter">
+                <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+                <label class="trip-filters__filter-label" for="filter-past">Past</label>
+              </div>
+
+              <button class="visually-hidden" type="submit">Accept filter</button>
+            </form>`
+  );
+};
+
+render(siteHeaderInfo, createRouteComponent(), `afterbegin`);
+render(siteHeaderMenu, createSiteMenuComponent());
+render(siteHeaderMenu, createSiteFilterComponent());
+
+
